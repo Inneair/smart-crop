c9a24371725186f0907913235ec82eda8a18df56
guillaume <guiogier@sfr.fr>
2015-10-08 14:37:25 +0200

    refactor views

---
+++ views/horizontalBalanced.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Crop horizontal</title>
+</head>
+<body>
+<table>
+    <?php
+    $images = scandir('img/');
+    foreach ($images as $image) {
+    list($width, $height, $type, $attr) = getimagesize("img/" . $image);
+    ?>
+    <tr style="text-align:center">
+        <td style="border:1px solid black">Original picture</td>
+        <td style="border:1px solid black">crop with Gd</td>
+        <td style="border:1px solid black">crop with Gmagick</td>
+        <td style="border:1px solid black">Crop with Imagick</td>
+    </tr>
+    <tr style="text-align:center">
+        <td> <?php echo $width ?> x
+            <?php echo $height ?>;</br>
+            <img src="img/<?php echo $image ?>" border="0" style="max-width: 200px"/>
+        </td>
+        <td><img src="tmp/Gd/carre/<?php echo $image ?>" border="0"/></td>
+        <td><img src="tmp/entropy2/carre/<?php echo $image ?>" border="0"/></td>
+        <td><img src="tmp/balance/carre/<?php echo $image ?>" border="0"/></td>
+    </tr>
+    <?php
+    }
+    ?>
+</table>
+
+<button style="cursor:pointer; font-weight:700;" type="button">
+    <a href="/index.php">Back to menu</a>
+</button>
+
+</body>
+</html>
