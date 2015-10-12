0f7004679492f990566c97e8c344b9cef70f60c5
Guillaume <guiogier@sfr.fr>
2015-10-12 10:42:33 +0200

    Update views

--- views/view.php
+++ views/view.php
@@ -8,70 +8,70 @@
     <!-- buttons for difference Balanced -->
     <h1>buttons for difference Balanced</h1>
     <button style="cursor:pointer; font-weight:700;" type="button">
-        <a href="squareBalanced.php">Crop square with all drivers</a>
+        <a href="/smart-crop/views/squareBalanced.php">Crop square with all drivers</a>
     </button>
 
     <button style="cursor:pointer; font-weight:700;" type="button">
-        <a href="horizontalBalanced.php">Crop horizontal with all drivers</a>
+        <a href="/smart-crop/views/horizontalBalanced.php">Crop horizontal with all drivers</a>
     </button>
 
     <button style="cursor:pointer; font-weight:700;" type="button">
-        <a href="verticalBalanced.php">Crop vertical with all drivers</a>
+        <a href="/smart-crop/views/verticalBalanced.php">Crop vertical with all drivers</a>
     </button>
 
     <!-- buttons for difference Entropy -->
     <h1>buttons for difference Entropy</h1>
     <button style="cursor:pointer; font-weight:700;" type="button">
-        <a href="squareEntropy.php">Crop square with all drivers</a>
+        <a href="/smart-crop/views/squareEntropy.php">Crop square with all drivers</a>
     </button>
 
     <button style="cursor:pointer; font-weight:700;" type="button">
-        <a href="horizontalEntropy.php">Crop horizontal with all drivers</a>
+        <a href="/smart-crop/views/horizontalEntropy.php">Crop horizontal with all drivers</a>
     </button>
 
     <button style="cursor:pointer; font-weight:700;" type="button">
-        <a href="verticalEntropy.php">Crop vertical with all drivers</a>
+        <a href="/smart-crop/views/verticalEntropy.php">Crop vertical with all drivers</a>
     </button>
 
     <!-- buttons for difference With Gd only -->
     <h1>buttons for difference With Gd only</h1>
     <button style="cursor:pointer; font-weight:700;" type="button">
-        <a href="squareGd.php">Crop square with Gd</a>
+        <a href="/smart-crop/views/squareGd.php">Crop square with Gd</a>
     </button>
 
     <button style="cursor:pointer; font-weight:700;" type="button">
-        <a href="horizontalGd.php">Crop horizontal with Gd</a>
+        <a href="/smart-crop/views/horizontalGd.php">Crop horizontal with Gd</a>
     </button>
 
     <button style="cursor:pointer; font-weight:700;" type="button">
-        <a href="verticalGd.php">Crop vertical with Gd</a>
+        <a href="/smart-crop/views/verticalGd.php">Crop vertical with Gd</a>
     </button>
 
     <!-- buttons for difference With Gmagick only -->
     <h1>buttons for difference With Gmagick only</h1>
     <button style="cursor:pointer; font-weight:700;" type="button">
-        <a href="squareGmagick.php">Crop square with Gmagick</a>
+        <a href="/smart-crop/views/squareGmagick.php">Crop square with Gmagick</a>
     </button>
 
     <button style="cursor:pointer; font-weight:700;" type="button">
-        <a href="horizontalGmagick.php">Crop horizontal with Gmagick</a>
+        <a href="/smart-crop/views/horizontalGmagick.php">Crop horizontal with Gmagick</a>
     </button>
 
     <button style="cursor:pointer; font-weight:700;" type="button">
-        <a href="verticalGmagick.php">Crop vertical with Gmagick</a>
+        <a href="/smart-crop/views/verticalGmagick.php">Crop vertical with Gmagick</a>
     </button>
 
     <!-- buttons for difference With Imagick only -->
     <h1>buttons for difference With Imagick only</h1>
     <button style="cursor:pointer; font-weight:700;" type="button">
-        <a href="squareImagick.php">Crop square with Imagick</a>
+        <a href="/smart-crop/views/squareImagick.php">Crop square with Imagick</a>
     </button>
 
     <button style="cursor:pointer; font-weight:700;" type="button">
-        <a href="horizontalImagick.php">Crop horizontal with Imagick</a>
+        <a href="/smart-crop/views/horizontalImagick.php">Crop horizontal with Imagick</a>
     </button>
 
     <button style="cursor:pointer; font-weight:700;" type="button">
-        <a href="verticalImagick.php">Crop vertical with Imagick</a>
+        <a href="/smart-crop/views/verticalImagick.php">Crop vertical with Imagick</a>
     </button>
 </body>
